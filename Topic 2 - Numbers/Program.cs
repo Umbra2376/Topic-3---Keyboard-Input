aa59409d3765b972ccba20a64c4fb6600794dab1
362572158 <362572158@2260I-L29-019>
2026-09-09 13:19:54 -0400

    Add project files.

---
+++ Topic 2 - Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_3___Keyboard_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1
+            int currentYear = DateTime.Now.Year, userAge, currentAge;
+            string userName;
+            Console.WriteLine("Hi there! What's your name?");
+            userName = Console.ReadLine();
+            Console.WriteLine("Ok and how old are you right now?");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            currentAge = currentYear - userAge;
+            Console.WriteLine($"Well {userName}, it seems you were born in {currentAge}. wooooow...");
+
+            //#2
+
+        }
+    }
+}
